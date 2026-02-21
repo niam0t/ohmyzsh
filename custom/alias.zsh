be7b17afc67f1e85eb53516394b5537a40c1da36
Niamot <58494481+niamotullah@users.noreply.github.com>
2026-02-21 14:15:46 +0600

    update zip alias

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -8,10 +8,10 @@ alias_if_available() {
         alias "$alias_name"="$target_command $extra_args"
         return 0
     else
-        echo "Note: $target_command not found, skipping alias '$alias_name'"
+        # echo "Note: $target_command not found, skipping alias '$alias_name'"
         return 1
     fi
 }
 
 alias_if_available nano nvim "--clean"
-alias_if_available unzip unar 
+# alias_if_available unzip unar 
